8d050e8cde943624aa61c4192dfe17b3587f209f
veronika <nika.kolanda@gmail.com>
2023-02-07 00:16:41 +0300

    combo box

--- laba7/Form1.Designer.cs
+++ laba7/Form1.Designer.cs
@@ -30,11 +30,18 @@
         {
             this.Removee = new System.Windows.Forms.Button();
             this.Replace = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.company = new System.Windows.Forms.TextBox();
+            this.age = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // Removee
             // 
-            this.Removee.Location = new System.Drawing.Point(118, 165);
+            this.Removee.Location = new System.Drawing.Point(115, 249);
             this.Removee.Name = "Removee";
             this.Removee.Size = new System.Drawing.Size(165, 77);
             this.Removee.TabIndex = 0;
@@ -44,7 +51,7 @@
             // 
             // Replace
             // 
-            this.Replace.Location = new System.Drawing.Point(461, 165);
+            this.Replace.Location = new System.Drawing.Point(439, 299);
             this.Replace.Name = "Replace";
             this.Replace.Size = new System.Drawing.Size(175, 77);
             this.Replace.TabIndex = 1;
@@ -52,16 +59,84 @@
             this.Replace.UseVisualStyleBackColor = true;
             this.Replace.Click += new System.EventHandler(this.Replace_Click);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(115, 99);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(165, 28);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(439, 99);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(175, 28);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // company
+            // 
+            this.company.Location = new System.Drawing.Point(439, 167);
+            this.company.Name = "company";
+            this.company.Size = new System.Drawing.Size(175, 26);
+            this.company.TabIndex = 4;
+            this.company.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // age
+            // 
+            this.age.Location = new System.Drawing.Point(439, 234);
+            this.age.Name = "age";
+            this.age.Size = new System.Drawing.Size(175, 26);
+            this.age.TabIndex = 5;
+            this.age.TextChanged += new System.EventHandler(this.age_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(439, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Enter company";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(439, 201);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Enter age";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(115, 166);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 8;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.age);
+            this.Controls.Add(this.company);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.Replace);
             this.Controls.Add(this.Removee);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,6 +144,13 @@
 
         private System.Windows.Forms.Button Removee;
         private System.Windows.Forms.Button Replace;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox company;
+        private System.Windows.Forms.TextBox age;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- laba7/Form1.cs
+++ laba7/Form1.cs
@@ -27,13 +27,23 @@ namespace laba7
                 XmlDocument xmlDoc = new XmlDocument();
                 xmlDoc.Load("D:\\people.xml");
 
-                // Remove node
-                XmlNode nodeToRemove = xmlDoc.SelectSingleNode("people/person[@name='Tom']");
-                nodeToRemove.ParentNode.RemoveChild(nodeToRemove);
+                //// Remove node
+                //XmlNode nodeToRemove = xmlDoc.SelectSingleNode("people/person[@name='Tom']");
+                //nodeToRemove.ParentNode.RemoveChild(nodeToRemove);
 
-                xmlDoc.Save("people_modified.xml");
+                //xmlDoc.Save("people_modified.xml");
 
-                MessageBox.Show("XML Processing complete.");
+                if (comboBox1.SelectedIndex >= 0)
+                {
+                    string selectedName = comboBox1.SelectedItem.ToString();
+                    XmlNode nodeToRemove = xmlDoc.SelectSingleNode("/people/person[@name='" + selectedName + "']");
+                    nodeToRemove.ParentNode.RemoveChild(nodeToRemove);
+
+                    xmlDoc.Save("people_modified.xml");
+                    MessageBox.Show("Node removed successfully.");
+                }
+
+                
             }
             catch (Exception ex) {MessageBox.Show(ex.Message); }
         }
@@ -43,14 +53,74 @@ namespace laba7
             XmlDocument xmlDoc = new XmlDocument();
             xmlDoc.Load("D:\\people.xml");
 
-            XmlNode nodeToReplace = xmlDoc.SelectSingleNode("/people/person[@name='Bob']/company");
-            XmlNode newNode = xmlDoc.CreateElement("company");
-            newNode.InnerText = "Apple";
-            nodeToReplace.ParentNode.ReplaceChild(newNode, nodeToReplace);
+            //XmlNode nodeToReplace = xmlDoc.SelectSingleNode("/people/person[@name='Bob']/company");
+            //XmlNode newNode = xmlDoc.CreateElement("company");
+            //newNode.InnerText = "Apple";
+            //nodeToReplace.ParentNode.ReplaceChild(newNode, nodeToReplace);
 
-            xmlDoc.Save("people_modified.xml");
+            //xmlDoc.Save("people_modified.xml");
 
-            MessageBox.Show("XML Processing complete.");
+            //MessageBox.Show("XML Processing complete.");
+
+            if (comboBox2.SelectedIndex >= 0)
+            {
+                string selectedName = comboBox2.SelectedItem.ToString();
+                XmlNode nodeToReplace = xmlDoc.SelectSingleNode("/people/person[@name='" + selectedName + "']");
+
+                XmlElement newNode = xmlDoc.CreateElement("person");
+                newNode.SetAttribute("name", selectedName);
+
+                XmlElement newCompany = xmlDoc.CreateElement("company");
+                newCompany.InnerText = company.Text.ToString();
+                newNode.AppendChild(newCompany);
+                textBox1.Text = newCompany.InnerText.ToString();
+
+                XmlElement newAge = xmlDoc.CreateElement("age");
+                newAge.InnerText = age.Text.ToString();
+                newNode.AppendChild(newAge);
+
+                nodeToReplace.ParentNode.ReplaceChild(newNode, nodeToReplace);
+
+                xmlDoc.Save("people_modified.xml");
+                MessageBox.Show("Node replaced successfully.");
+            }
+        
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.DropDownStyle= ComboBoxStyle.DropDownList;
+            comboBox2.DropDownStyle= ComboBoxStyle.DropDownList;
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("D:\\people.xml");
+
+            XmlNode parentNode = xmlDoc.SelectSingleNode("/people");
+
+            foreach (XmlNode childNode in parentNode.ChildNodes)
+            {
+                comboBox1.Items.Add(childNode.Attributes["name"].Value);
+                comboBox2.Items.Add(childNode.Attributes["name"].Value);
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void age_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
